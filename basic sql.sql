6e9a3469cf7301ba6d2c4bb1c8b1c690621384c1
Sneha Saini <snehasaini4362@gmail.com>
2026-05-25 15:02:30 +0530

    Add files via upload

---
+++ basic sql.sql
@@ -0,0 +1,21 @@
+drop database sakila;
+use sakila;
+
+show tables;
+select * from actor;
+create database school;
+use  school;
+create table student (id int primary key,name varchar(50),age int not null);
+insert into student value(1,"sneha",20);
+insert into student value(2,"vartika",20);
+insert into student value(3,"chetna",21);
+insert into student value(4,"uday",19);
+insert into student value(5,"nitesh",22);
+select * from student;
+desc student ;
+show tables;
+
+
+
+
+
